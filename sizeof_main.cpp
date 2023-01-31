36c19d7435f09f990417b769282603c1d59e1e0e
david mckelvie <69944671+mckelvied@users.noreply.github.com>
2023-01-31 03:00:37 +0000

    vscode stuff

---
+++ sizeof_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main(){
+    cout << "int size: " << sizeof(int) << endl;
+    cout << "int MIN:" << INT_MIN << endl;
+    cout << "int MAX:" << INT_MAX << endl;
+    return 0;
+}
